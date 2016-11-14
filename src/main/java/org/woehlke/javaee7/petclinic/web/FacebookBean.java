ca56d6e3a5a0058ab3c370990b50d304cca95883
AlTavares <alexandre@live.in>
2016-11-14 14:27:46 -0200

    Create FacebookBean

---
+++ src/main/java/org/woehlke/javaee7/petclinic/web/FacebookBean.java
@@ -0,0 +1,55 @@
+/**
+ * Created by tavares on 13/11/16.
+ */
+
+package org.woehlke.javaee7.petclinic.web;
+
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+@ManagedBean(name = "facebookSession")
+@SessionScoped
+public class FacebookBean implements Serializable {
+
+    private static Logger log = Logger.getLogger(FacebookBean.class.getName());
+
+    public Boolean getLogged() throws FacebookException {
+        Object faceSession = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("facebook");
+
+        if (faceSession == null) {
+            return false;
+        }
+
+        Facebook facebook = (Facebook) faceSession;
+
+        log.info(facebook.toString());
+
+        return (facebook != null);
+
+    }
+
+    public String getName() throws FacebookException {
+        Object faceSession = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("facebook");
+
+        if (faceSession == null) {
+            return "";
+        }
+
+        Facebook facebook = (Facebook) faceSession;
+    log.info("\n\n\n\n\n");
+        log.info(facebook.toString());
+        log.info("\n\n\n\n\n");
+
+
+        if (facebook == null) {
+            return "";
+        }
+        return facebook.getName();
+    }
+}
