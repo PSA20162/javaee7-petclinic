aa844dd60e052be5ecb34b35cdea4f44d5c3158b
AlTavares <alexandre@live.in>
2016-11-14 14:46:51 -0200

    Add login to main page

--- src/main/java/org/woehlke/javaee7/petclinic/facebook/CallbackServlet.java
+++ src/main/java/org/woehlke/javaee7/petclinic/facebook/CallbackServlet.java
@@ -2,6 +2,8 @@ package org.woehlke.javaee7.petclinic.facebook;
 
 import facebook4j.Facebook;
 import facebook4j.FacebookException;
+import facebook4j.Reading;
+import facebook4j.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,11 +18,17 @@ public class CallbackServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Facebook facebook = (Facebook) request.getSession().getAttribute("facebook");
         String oauthCode = request.getParameter("code");
+        saveLogin(facebook);
         try {
             facebook.getOAuthAccessToken(oauthCode);
         } catch (FacebookException e) {
             throw new ServletException(e);
         }
         response.sendRedirect(request.getContextPath() + "/");
+    }
+
+    private void saveLogin(Facebook facebook) {
+        User user = facebook.getMe()
+                
     }
 }
